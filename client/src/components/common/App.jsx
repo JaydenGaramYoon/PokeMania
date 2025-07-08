c6a27b07c2b485f74e3243db6c542e53ec3c8a69
Garam Yoon <rkfka9536@gmail.com>
2025-07-08 04:54:52 -0400

    Rearrange folder structure

--- client/src/components/common/App.jsx
+++ client/src/components/common/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import MainRouter from '../MainRouter';
+import MainRouter from '../../../MainRouter';
 const App = () => {
   return (
     <Router>
